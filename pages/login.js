58d2aabea5630544516861dbd0d03f49b535f834
amardeus02-sudo <amardeus02@gmail.com>
2026-03-18 18:28:14 -0300

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
- sb_publishable_cWTy7Bccrdn_Bn0kZY_-wQ_8nDVbWoX,
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3YnZyaHFleWNva2lvand6dWdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NTkzMjMsImV4cCI6MjA4OTQzNTMyM30.oOeX804nsZbggFhKmNMfappHtzOnwcnRYxqTlq75bVk
+ "sb_publishable_cWTy7Bccrdn_Bn0kZY_-wQ_8nDVbWoX",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3YnZyaHFleWNva2lvand6dWdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NTkzMjMsImV4cCI6MjA4OTQzNTMyM30.oOeX804nsZbggFhKmNMfappHtzOnwcnRYxqTlq75bVk"
 );
 
 export default function Login() {
