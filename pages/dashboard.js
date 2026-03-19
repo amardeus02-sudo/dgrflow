c01c170cfb533179d4d1481c703dd3c4d8332291
amardeus02-sudo <amardeus02@gmail.com>
2026-03-19 15:13:37 -0300

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,59 +1,22 @@
-import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
+<div style={{ marginBottom: 10 }}>
+  <a
+    href={`https://wwbvrhqeycokiojwzugg.supabase.co/storage/v1/object/public/sds-files/${job.file_name}`}
+    target="_blank"
+  >
+    📄 Download SDS
+  </a>
+</div>
 
-const supabase = createClient(
-  "https://wwbvrhqeycokiojwzugg.supabase.co",
-  "sb_publishable_cWTy7Bccrdn_Bn0kZY_-wQ_8nDVbWoX"
-);
+<div style={{ marginBottom: 10 }}>
+  <button onClick={() => updateStatus(job.id, "pending")}>
+    🔴 Pending
+  </button>
 
-export default function Dashboard() {
-  const [jobs, setJobs] = useState([]);
+  <button onClick={() => updateStatus(job.id, "in progress")}>
+    🟡 In Progress
+  </button>
 
-  useEffect(() => {
-    fetchJobs();
-  }, []);
-
-  async function fetchJobs() {
-    const { data } = await supabase.from("jobs").select("*");
-    setJobs(data || []);
-  }
-
-  async function updateStatus(id, status) {
-    await supabase.from("jobs").update({ status }).eq("id", id);
-    fetchJobs();
-  }
-
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>TESTE DASHBOARD</h1>
-
-      {jobs.map((job) => (
-        <div key={job.id} style={{ marginBottom: 20 }}>
-       <p>
-  <strong>{job.product_name}</strong> - {job.status}
-</p>
-
-<a
-  href={`https://wwbvrhqeycokiojwzugg.supabase.co/storage/v1/object/public/sds-files/${job.file_name}`}
-  target="_blank"
->
-  📄 Download SDS
-</a>
-
-          {/* BOTÕES GRANDES */}
-          <button onClick={() => updateStatus(job.id, "pending")}>
-            🔴 Pending
-          </button>
-
-          <button onClick={() => updateStatus(job.id, "in progress")}>
-            🟡 In Progress
-          </button>
-
-          <button onClick={() => updateStatus(job.id, "done")}>
-            🟢 Done
-          </button>
-        </div>
-      ))}
-    </div>
-  );
-}
+  <button onClick={() => updateStatus(job.id, "done")}>
+    🟢 Done
+  </button>
+</div>
