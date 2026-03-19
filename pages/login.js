8447ee8db32ef0b1a16e7d732f18270ac88d98b4
amardeus02-sudo <amardeus02@gmail.com>
2026-03-19 18:04:15 -0300

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,27 +1,53 @@
 import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
+import { useRouter } from "next/router";
 
 const supabase = createClient(
- "https://wwbvrhqeycokiojwzugg.supabase.co",
+  "https://wwbvrhqeycokiojwzugg.supabase.co",
   "sb_publishable_cWTy7Bccrdn_Bn0kZY_-wQ_8nDVbWoX"
 );
 
 export default function Login() {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
 
-  const handleLogin = async () => {
-    await supabase.auth.signInWithOtp({ email });
-    alert("Check your email!");
-  };
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert("Erro no login");
+    } else {
+      router.push("/dashboard");
+    }
+  }
 
   return (
     <div style={{ padding: 40 }}>
-      <h2>Login</h2>
-      <input
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+      <h1>Login</h1>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br /><br />
+
+        <button type="submit">Entrar</button>
+      </form>
     </div>
   );
 }
