a5f206310101bcac87abc7ef830a7c0cfc94e52d
amardeus02-sudo <amardeus02@gmail.com>
2026-04-30 14:39:08 -0300

    Change import paths for components in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Features from "@/components/Features";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
 
 export default function Home() {
   return (
